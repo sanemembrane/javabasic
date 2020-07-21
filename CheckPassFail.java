b25f0be350f52bc3aa09133b80bdfa3369f21bd8
sanemembrane <68488747+sanemembrane@users.noreply.github.com>
2020-07-20 21:37:31 -0600

    Add files via upload

---
+++ CheckPassFail.java
@@ -0,0 +1,16 @@
+package com.busyqa.coursework.wk1_answer;
+
+public class CheckPassFail {  // Save as "CheckPassFail.java"
+    public static void main(String[] args) {  // Program entry point
+        
+    	int mark = 51;  // Set the value of "mark" here!
+        System.out.println("The mark is " + mark);
+
+        if (mark >= 50) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+        System.out.println( "DONE" );
+    }
+}
